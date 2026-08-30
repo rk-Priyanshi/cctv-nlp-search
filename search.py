5489a3689e0118bcbbda074e21f95f71a2d6c6b2
Priyanshi <priyanshi.sharma.workspirit@gmail.com>
2026-08-30 17:06:54 +0530

    Update Qdrant client connection (Secure)

--- search.py
+++ search.py
@@ -1,10 +1,10 @@
 from qdrant_client import QdrantClient
 from embedder import get_text_embedding
 
-# Connect to Qdrant
+import streamlit as st
 client = QdrantClient(
-    url="https://81ee74e6-44fe-49f2-a7d7-4e5b2c4191b9.us-east-2-0.aws.cloud.qdrant.io",
-    api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIiwic3ViamVjdCI6ImFwaS1rZXk6MGQwMWM2N2QtODdlMy00Yjg2LWE3MjAtMjAzYzJkNWM0ZTBlIn0.6QqREzZi_shEIDoxmqBGS7xUc8AK-tpbgdDqZi1i0bI",
+    url=st.secrets["QDRANT_URL"],
+    api_key=st.secrets["QDRANT_API_KEY"]
 )
 COLLECTION_NAME = "cctv_frames"
 
